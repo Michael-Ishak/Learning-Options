8bd01aa85e9d019ea1c85505e89352b68858e69f
Michael <merelyquant@users.noreply.github.com>
2025-01-24 02:11:03 +0000

    moved main

--- src/BlackScholes.cpp
+++ src/BlackScholes.cpp
@@ -59,12 +59,12 @@ public:
     }
 };
 
-int main() {
-    Call callOption(100, 100, 0.05, 0.2, 1);
-    std::cout << "Call Price: " << callOption.price() << std::endl;
+// int main() {
+//     Call callOption(100, 100, 0.05, 0.2, 1);
+//     std::cout << "Call Price: " << callOption.price() << std::endl;
 
-    Put putOption(100, 100, 0.05, 0.2, 1);
-    std::cout << "Put Price: " << putOption.price() << std::endl;
+//     Put putOption(100, 100, 0.05, 0.2, 1);
+//     std::cout << "Put Price: " << putOption.price() << std::endl;
 
-    return 0;
-}
+//     return 0;
+// }
