2704f51ce19de9fb8e9066c32ab74a7e599cbf03
Michael <merelyquant@users.noreply.github.com>
2025-01-24 02:04:39 +0000

    optimised and improved upon abstraction etc

--- src/BlackScholes.cpp
+++ src/BlackScholes.cpp
@@ -4,78 +4,67 @@
 
 class NumericalOperations{
     protected:
-        static double exp_minus_t_squared(double t) {
+        static inline double exp_minus_t_squared(double t) {
             return std::exp(-t * t);
     }
-        static double integrate(double a, double b, int num_intervals = 10000) {
-            double step = (b - a) / num_intervals;
-            double integral = 0.0;
-
-            for (int i = 0; i < num_intervals; ++i) {
-                double t1 = a + i * step;
-                double t2 = a + (i + 1) * step;
-                integral += 0.5 * (exp_minus_t_squared(t1) + exp_minus_t_squared(t2)) * step;
-            }
-            return integral;
-        }
-        static double erf(double x) {
-            return std::erf(x);
-        }
         static double NormalDistributionCDF(double x){
-            return 0.5 * (1 + erf(x));
+            return 0.5 * (1 + std::erf(x));
         }
 };
 
 class Option: protected NumericalOperations{
     protected:
-        double s_0;                     //  Current stock price
-        double K;                       //  Strike price
-        double r;                        // Risk-free rate
-        double sigma;                    //  Volatility of the underlying
-        double T;                        //  Time to maturity in years
+        double s_0, K, r, sigma, T;
     
     public:
         Option(double s_0, double K, double r, double sigma, double T) : s_0(s_0), K(K), r(r), sigma(sigma), T(T) {}
 
+        virtual ~Option() {};
+
+        virtual double price() const = 0;
     protected:
-        double d_one(){
+        [[nodiscard]] double d_one() const {
             return (std::log(s_0/K) + T * (r + std::pow(sigma, 2))/2)/(sigma * std::sqrt(T));
         }
-        double d_two(){
+       [[nodiscard]] double d_two() const {
             return d_one() - (sigma * std::sqrt(T));
         }
 };
 
-class Call: protected Option{
-    public:
-        Call(double s_0, double K, double r, double sigma, double T)
-            : Option(s_0, K, r, sigma, T) {}
+class Call : public Option {
+public:
+    Call(double s_0, double K, double r, double sigma, double T)
+        : Option(s_0, K, r, sigma, T) {}
 
-        double call(){
-            double d1 = d_one();
-            double d2 = d_two();
-            return s_0 * NormalDistributionCDF(d1) - K*std::exp(-r * T)*NormalDistributionCDF(d2);
-        }
+    [[nodiscard]] double call() const { return price(); }
+
+    [[nodiscard]] double price() const override{
+        double d1 = d_one();
+        double d2 = d_two();
+        return s_0 * NormalDistributionCDF(d1) - K * std::exp(-r * T) * NormalDistributionCDF(d2);
+    }
 };
 
-class Put: protected Option{
-    public:
-        Put(double s_0, double K, double r, double sigma, double T)
+class Put : public Option {
+public:
+    Put(double s_0, double K, double r, double sigma, double T)
         : Option(s_0, K, r, sigma, T) {}
-        
-        double put(){
-            double d1 = d_one();
-            double d2 = d_two();
-            return K*std::exp(-r * T)*NormalDistributionCDF(-d2) - s_0 * NormalDistributionCDF(-d1);
-        }
+
+    [[nodiscard]] double put() const { return price(); }
+
+    [[nodiscard]] double price() const override{
+        double d1 = d_one();
+        double d2 = d_two();
+        return K * std::exp(-r * T) * NormalDistributionCDF(-d2) - s_0 * NormalDistributionCDF(-d1);
+    }
 };
 
 int main() {
     Call callOption(100, 100, 0.05, 0.2, 1);
-    std::cout << "Call Price: " << callOption.call() << std::endl;
+    std::cout << "Call Price: " << callOption.price() << std::endl;
 
     Put putOption(100, 100, 0.05, 0.2, 1);
-    std::cout << "Put Price: " << putOption.put() << std::endl;
+    std::cout << "Put Price: " << putOption.price() << std::endl;
 
     return 0;
 }
