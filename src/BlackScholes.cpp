e8688e7e72ef8c44bb4a7244752a0032391f325f
Michael <merelyquant@users.noreply.github.com>
2025-01-24 01:40:12 +0000

    fixed ambiguity with namespacing

--- src/BlackScholes.cpp
+++ src/BlackScholes.cpp
@@ -22,7 +22,7 @@ class NumericalOperations{
             return std::erf(x);
         }
         double NormalDistributionCDF(double x){
-            return f_x = 0.5 * (1 + erf(x));
+            return 0.5 * (1 + erf(x));
         }
 };
 
@@ -39,27 +39,43 @@ class Option: protected NumericalOperations{
 
     protected:
         double d_one(){
-            return (log(s_0/K) + T * (r + pow(sigma, 2))/2)/(sigma * sqrt(T));
+            return (std::log(s_0/K) + T * (r + std::pow(sigma, 2))/2)/(sigma * std::sqrt(T));
         }
         double d_two(){
-            return d_one() - (sigma * sqrt(T));
+            return d_one() - (sigma * std::sqrt(T));
         }
 };
 
 class Call: protected Option{
     public:
+        Call(double s_0, double K, double r, double sigma, double T)
+            : Option(s_0, K, r, sigma, T) {}
+
         double call(){
             double d1 = d_one();
             double d2 = d_two();
-            return s_0 * NormalDistributionCDF(d1) - K*exp(-r * T)*NormalDistributionCDF(d2);
+            return s_0 * NormalDistributionCDF(d1) - K*std::exp(-r * T)*NormalDistributionCDF(d2);
         }
 };
 
 class Put: protected Option{
     public:
+        Put(double s_0, double K, double r, double sigma, double T)
+        : Option(s_0, K, r, sigma, T) {}
+        
         double put(){
             double d1 = d_one();
             double d2 = d_two();
-            return K*exp(-r * T)*NormalDistributionCDF(-d2) - s_0 * NormalDistributionCDF(-d1);
+            return K*std::exp(-r * T)*NormalDistributionCDF(-d2) - s_0 * NormalDistributionCDF(-d1);
         }
 };
+
+int main() {
+    Call callOption(100, 100, 0.05, 0.2, 1);
+    std::cout << "Call Price: " << callOption.call() << std::endl;
+
+    Put putOption(100, 100, 0.05, 0.2, 1);
+    std::cout << "Put Price: " << putOption.put() << std::endl;
+
+    return 0;
+}
