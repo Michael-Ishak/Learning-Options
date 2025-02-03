0a8b922e15f884b67030fa2a9dcba703b596b6a2
Michael Ishak <ishak.michael3@gmail.com>
2025-02-03 22:14:03 +0000

    fixed imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include "BlackScholes.cpp"
+#include "BlackScholes.hpp"
 
 using namespace std;
 
 int main() {
     Call callOption(100, 100, 0.05, 0.2, 1);
     std::cout << "Call Price: " << callOption.price() << std::endl;
+    std::cout << "Delta: " << callOption.delta() << std::endl;
+    std::cout << "Gamma: " << callOption.gamma() << std::endl;
+    std::cout << "Theta: " << callOption.theta() << std::endl;
+    std::cout << "Vega: " << callOption.vega() << std::endl;
+    std::cout << "Rho: " << callOption.rho() << std::endl;
 
     Put putOption(100, 100, 0.05, 0.2, 1);
     std::cout << "Put Price: " << putOption.price() << std::endl;
+    std::cout << "Delta: " << putOption.delta() << std::endl;
+    std::cout << "Gamma: " << putOption.gamma() << std::endl;
+    std::cout << "Theta: " << putOption.theta() << std::endl;
+    std::cout << "Vega: " << putOption.vega() << std::endl;
+    std::cout << "Rho: " << putOption.rho() << std::endl;
 
     return 0;
 }
